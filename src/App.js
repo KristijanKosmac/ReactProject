ff41d530a5b0efbca5d53a5daada29a00fae26e1
Kristijan <kos_kiki@hotmail.com>
2020-08-31 16:42:12 +0200

    added Homepage, Directory and styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import HomePage from "./homepage.component";
+import HomePage from "./pages/homepage/home-page.component";
 
 function App() {
   return <HomePage></HomePage>;
