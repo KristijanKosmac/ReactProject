7cba9161e834e2ef85d2bd35de95d2d22d2c7546
Kristijan <kos_kiki@hotmail.com>
2020-09-14 12:04:29 +0200

    adding remove button functionality and increasing/decreasing button functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/home-page.component";
+import CheckoutPage from "./pages/checkout/checkout.component";
 import { Route, Link, Switch, Redirect } from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header.component/header.component";
@@ -75,6 +76,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage}></Route>
           <Route exact path="/shop" component={ShopPage}></Route>
+          <Route exact path="/checkout" component={CheckoutPage} exact></Route>
           <Route
             exact
             path="/signin"
--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,21 +1,37 @@
 import React from "react";
 import { connect } from "react-redux";
 import CustomButton from "../custom-button/custom-button.component";
+import { withRouter } from "react-router-dom";
 
 import "./cart-dropdown.styles.scss";
 import CartItem from "../cart-item/cart-item.component";
 import { selectCartItems } from "../../redux/cart/cart.selectors";
-
+import { toggleCartHidden } from "../../redux/cart/cart.actions";
 import { createStructuredSelector } from "reselect";
 
-const CartDropdown = ({ cartItems }) => (
+// Moze i da se destruktira i namesto mapDispatchToProps da se pisuva moze samo kako argument da se dade namesto toggleCartHidden,
+//dispatch i posle vo onClick namesto toggleCartHidden kje bide dispatch(toggleCartHidden());
+const CartDropdown = ({ cartItems, history, toggleCartHidden }) => (
   <div className="cart-dropdown ">
     <div className="cart-items">
-      {cartItems.map((cartItem) => (
-        <CartItem key={cartItem.id} item={cartItem}></CartItem>
-      ))}
+      {cartItems.length ? (
+        cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} item={cartItem}></CartItem>
+        ))
+      ) : (
+        <span className="empty-message">Your cart is empty</span>
+      )}
     </div>
-    <CustomButton>CHECKOUT</CustomButton>
+    <CustomButton
+      onClick={() => {
+        history.push("/checkout");
+        {
+          toggleCartHidden();
+        }
+      }}
+    >
+      CHECKOUT
+    </CustomButton>
   </div>
 );
 
@@ -34,4 +50,10 @@ const mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems,
 });
 
-export default connect(mapStateToProps)(CartDropdown);
+const mapDispatchToProps = (dispatch) => ({
+  toggleCartHidden: () => dispatch(toggleCartHidden()),
+});
+
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(CartDropdown)
+);
